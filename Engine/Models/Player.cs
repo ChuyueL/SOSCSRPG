9e655b0707fc82b2ac5cb547732fc10e6c5733c7
ChuyueL <chuyueluo@gmail.com>
2020-05-31 20:01:41 +0100

    Added property change alerts

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,18 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged //any change to property alerts other classes that care about this property
     {
-        public string Name { get; set; } //we can get and set this property 
-        public string CharacterClass { get; set; }
-        public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
-        public int Level { get; set; }
-        public int Gold { get; set; }
+        private string _name;
+        private string _characterClass;
+        private int _hitPoints;
+        private int _experiencePoints; //private backing variable
+        private int _level;
+        private int _gold;
+        public string Name 
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        } //we can get and set this property 
+        public string CharacterClass 
+        { 
+            get { return _characterClass; }
+            set 
+            { 
+                _characterClass = value;
+                OnPropertyChanged("CharacterClass");
+            }
+        }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set 
+            { 
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+        public int ExperiencePoints { //property
+            get { return _experiencePoints; }
+            set { 
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+        public int Level 
+        { 
+            get { return _level; }
+            set 
+            { 
+                _level = value;
+                OnPropertyChanged("Level");
+            }
+        }
+        public int Gold 
+        {   get { return _gold}
+            set 
+            {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
