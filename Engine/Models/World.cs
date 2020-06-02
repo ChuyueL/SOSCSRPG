41167800b5abae941f2c9ae8495a3f97f96ee763
ChuyueL <chuyueluo@gmail.com>
2020-06-02 19:58:52 +0100

    Added game world and movement within game world

---
+++ Engine/Models/World.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        private List<Location> _locations = new List<Location>(); //can only be accessed in World
+
+        internal void AddLocation(int xCoordinate, int yCoordinate, string name, string description, string imageName)
+        {
+            Location loc = new Location();
+            loc.XCoordinate = xCoordinate;
+            loc.YCoordinate = yCoordinate;
+            loc.Name = name;
+            loc.Description = description;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc);
+        }
+
+        //Returns the Location objects at the x-coordinate and y-coordinate provided.
+        public Location LocationAt(int xCoordinate, int yCoordinate) //need to call it from UI project so public
+        {
+            foreach (Location loc in _locations) 
+            {
+                if (loc.XCoordinate == xCoordinate && loc.YCoordinate == yCoordinate)
+                {
+                    return loc;
+                }
+
+            }
+
+            return null;
+        }
+    }
+}
